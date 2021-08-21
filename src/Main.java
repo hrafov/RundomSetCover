cbab8360f96b9275de5dce08059da3c805801dfb
grafov <hrafov@gmail.com>
2021-08-21 14:51:35 +0300

    16 points set

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Main {
     public static void main(String... args) {
 
-        int numberOfVertices = 5;
-        int numberOfIterations = 50;
+        int numberOfVertices = 16;
+        int numberOfIterations = 5000;
 
         ArrayList<ArrayList<Integer>> adj = inputInit(numberOfVertices);
 
@@ -34,18 +34,110 @@ public class Main {
         System.out.println("\n minSetCover = " + minSetCover + " for " + numberOfIterations + " iterations");
     }
 
+//    static ArrayList<ArrayList<Integer>> inputInit(int v) {
+//        // Creating a graph with 5 vertices - https://www.geeksforgeeks.org/graph-and-its-representations/
+//        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(v);
+//        for (int i = 0; i < v; i++)
+//            adj.add(new ArrayList<>());
+//        addEdge(adj, 0, 1);
+//        addEdge(adj, 0, 4);
+//        addEdge(adj, 1, 2);
+//        addEdge(adj, 1, 3);
+//        addEdge(adj, 1, 4);
+//        addEdge(adj, 2, 3);
+//        addEdge(adj, 3, 4);
+//        return adj;
+//    }
+
     static ArrayList<ArrayList<Integer>> inputInit(int v) {
-        // Creating a graph with 5 vertices - https://www.geeksforgeeks.org/graph-and-its-representations/
+        // Creating a graph with 16 vertices - https://www.youtube.com/watch?v=XmW3xR-0CSE
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>(v);
         for (int i = 0; i < v; i++)
             adj.add(new ArrayList<>());
         addEdge(adj, 0, 1);
         addEdge(adj, 0, 4);
+        addEdge(adj, 0, 3);
+
+        addEdge(adj, 1, 0);
         addEdge(adj, 1, 2);
-        addEdge(adj, 1, 3);
+        addEdge(adj, 1, 5);
         addEdge(adj, 1, 4);
-        addEdge(adj, 2, 3);
+
+        addEdge(adj, 2, 1);
+        addEdge(adj, 2, 6);
+        addEdge(adj, 2, 5);
+
+        addEdge(adj, 3, 0);
         addEdge(adj, 3, 4);
+        addEdge(adj, 3, 7);
+        addEdge(adj, 3, 10);
+        addEdge(adj, 3, 9);
+
+        addEdge(adj, 4, 0);
+        addEdge(adj, 4, 1);
+        addEdge(adj, 4, 5);
+        addEdge(adj, 4, 7);
+        addEdge(adj, 4, 3);
+
+        addEdge(adj, 5, 1);
+        addEdge(adj, 5, 2);
+        addEdge(adj, 5, 6);
+        addEdge(adj, 5, 8);
+        addEdge(adj, 5, 7);
+        addEdge(adj, 5, 4);
+
+        addEdge(adj, 6, 2);
+        addEdge(adj, 6, 12);
+        addEdge(adj, 6, 8);
+        addEdge(adj, 6, 5);
+
+        addEdge(adj, 7, 3);
+        addEdge(adj, 7, 4);
+        addEdge(adj, 7, 5);
+        addEdge(adj, 7, 8);
+        addEdge(adj, 7, 11);
+        addEdge(adj, 7, 10);
+
+        addEdge(adj, 8, 5);
+        addEdge(adj, 8, 6);
+        addEdge(adj, 8, 12);
+        addEdge(adj, 8, 11);
+        addEdge(adj, 8, 7);
+
+        addEdge(adj, 9, 3);
+        addEdge(adj, 9, 10);
+        addEdge(adj, 9, 13);
+
+        addEdge(adj, 10, 3);
+        addEdge(adj, 10, 7);
+        addEdge(adj, 10, 11);
+        addEdge(adj, 10, 13);
+        addEdge(adj, 10, 9);
+
+        addEdge(adj, 11, 7);
+        addEdge(adj, 11, 8);
+        addEdge(adj, 11, 12);
+        addEdge(adj, 11, 14);
+        addEdge(adj, 11, 10);
+
+        addEdge(adj, 12, 8);
+        addEdge(adj, 12, 6);
+        addEdge(adj, 12, 15);
+        addEdge(adj, 12, 14);
+        addEdge(adj, 12, 11);
+
+        addEdge(adj, 13, 9);
+        addEdge(adj, 13, 10);
+        addEdge(adj, 13, 14);
+
+        addEdge(adj, 14, 11);
+        addEdge(adj, 14, 12);
+        addEdge(adj, 14, 15);
+        addEdge(adj, 14, 13);
+
+        addEdge(adj, 15, 12);
+        addEdge(adj, 15, 14);
+
         return adj;
     }
 
